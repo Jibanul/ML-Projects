66d624d8b541254a18f1ee3e0efca4f15be67174
Md Jibanul Haque Jiban <jibanul@knights.ucf.edu>
2021-01-31 23:14:45 -0500

    Add files via upload
    Python Script for hw1

---
+++ Homework1/hw1.py
@@ -0,0 +1,16 @@
+# Author: Md Jibanul Haque Jiban
+# Homework 1
+# CAP 5610: Machine Learning
+
+# required libraries
+import numpy as np
+import pandas as pd
+
+
+# import data
+train_df = pd.read_csv('/Users/mdjibanulhaquejiban/PhD_CRCV/Semesters/Spring2021/ML/HW/HW1/Titanic/test.csv')
+test_df = pd.read_csv('/Users/mdjibanulhaquejiban/PhD_CRCV/Semesters/Spring2021/ML/HW/HW1/Titanic/test.csv')
+combine = [train_df, test_df]
+
+# Q1
+
